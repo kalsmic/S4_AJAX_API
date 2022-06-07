c72a3048cd530a18ace28dd505afda25bcd6dd8e
Arthur Kalule <kalulearthur@gmail.com>
2022-06-07 10:29:36 +0300

    format comment

--- helpers.py
+++ helpers.py
@@ -16,5 +16,6 @@ def validate_request(func):
 
     return wrapper
 
-# Note: A decorator is a design pattern in Python that allows a user to add new functionality to an existing object without
-# modifying its structure. Decorators are usually called before the definition of a function you want to decorate.
+# Note: A decorator is a design pattern in Python that allows a user to add new functionality to an existing object
+# without modifying its structure. Decorators are usually called before the definition of a function you want to
+# decorate.
