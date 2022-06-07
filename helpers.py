88abcd5007b6423319b4b582c347b65916104839
Arthur Kalule <kalulearthur@gmail.com>
2022-06-07 10:19:46 +0300

    add custom decorator

---
+++ helpers.py
@@ -0,0 +1,20 @@
+from functools import wraps
+
+from flask import request, make_response, jsonify
+from werkzeug.exceptions import abort
+
+
+# This is a decorator function we have written to validate the request
+# payload
+def validate_request(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        data = request.get_json()
+        if not len(data) or data.get('name', None) == None:
+            abort(make_response(jsonify(message='Please provide an name property '), 400))
+        return func(*args, **kwargs, )
+
+    return wrapper
+
+# Note: A decorator is a design pattern in Python that allows a user to add new functionality to an existing object without
+# modifying its structure. Decorators are usually called before the definition of a function you want to decorate.
